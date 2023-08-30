dd0393ff998ac5c27c3d599cc610802d85b527f3
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-08-30 09:21:36 +0900

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninGoogleComponent } from './account/signin-google/signin-google.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninGoogleComponent
   ],
   imports: [
     BrowserModule,
