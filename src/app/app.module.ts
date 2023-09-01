22a8d07f258a4922112fc36893baba7bd1977598
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-09-01 09:05:24 +0900

    angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { CalendarModule } from '@syncfusion/ej2-angular-calendars';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SigninGoogleComponent } from './account/signin-google/signin-google.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { SigninGoogleComponent } from './account/signin-google/signin-google.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CalendarModule
+    CalendarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/calendar.ts
@@ -0,0 +1,33 @@
+import {Component} from '@angular/core';
+import {FormGroup, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+const today = new Date();
+const month = today.getMonth();
+const year = today.getFullYear();
+
+@Component({
+  selector: 'date-range-picker-comparison-example',
+  templateUrl: 'calendar.html',
+  styleUrls: ['app.component.css'],
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+})
+export class DateRangePickerComparisonExample {
+  campaignOne = new FormGroup({
+    start: new FormControl(new Date(year, month, 13)),
+    end: new FormControl(new Date(year, month, 16)),
+  });
+  campaignTwo = new FormGroup({
+    start: new FormControl(new Date(year, month, 15)),
+    end: new FormControl(new Date(year, month, 19)),
+  });
+}
