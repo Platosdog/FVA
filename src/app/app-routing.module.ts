60daf6af4cc382347ebae941d08f711ebc2721c5
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-09-01 09:23:05 +0900

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Calendar } from '@syncfusion/ej2-angular-calendars';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'calendar', component: Calendar}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
