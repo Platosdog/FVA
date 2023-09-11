efe7ef5162821ebc58c77c49bb36510cbfeee95a
Platosdog <62915572+Platosdog@users.noreply.github.com>
2023-09-11 16:56:14 +0900

    routing problems

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Calendar } from '@syncfusion/ej2-angular-calendars';
 
 const routes: Routes = [
-  {path: 'calendar', component: Calendar}
+  {path: 'index',
+  component: Component,
+  children: [
+    {
+      path: 'calendar',
+      component: Calendar,
+    },
+    {
+
+    }
+
+  ]
+}
 ];
 
 @NgModule({
